95008fc1070095a605a094cc1ed500cfb5d4a453
Mahmoud Samy <mahmodsamy102@gmail.com>
2020-11-08 14:06:29 +0200

    Add project files.

---
+++ SamuraiApp.Domain/Clan.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SamuraiApp.Domain
+{
+    public class Clan
+    {
+        public int ClanId { get; set; }
+        public string ClanName { get; set; }
+    }
+}
---
+++ SamuraiApp.Domain/Quote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamuraiApp.Domain
+{
+   public class Quote
+    {
+        public int QuoteId { get; set; }
+        public string QuotesText { get; set; }
+        public Samurai Samurai { get; set; }
+        public int SamuraiId { get; set; }
+    }
+}
