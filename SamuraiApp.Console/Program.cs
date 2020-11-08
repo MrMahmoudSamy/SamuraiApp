a505d629b4e84a4bee7f59bed5ef45a2a6e3a316
Mahmoud Samy <mahmodsamy102@gmail.com>
2020-11-08 14:33:59 +0200

    Setup EntityFrameworkCore In Data

--- SamuraiApp.Console/Program.cs
+++ SamuraiApp.Console/Program.cs
@@ -6,7 +6,7 @@ namespace SamuraiApp.Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Console.WriteLine("Hello World!");
         }
     }
 }
